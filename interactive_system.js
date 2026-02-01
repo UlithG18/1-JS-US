31defd185270420b5d73ce295f3b3a896993bfa1
UlithG18 <ulith.software@gmail.com>
2026-02-01 02:26:53 -0500

    feat: all the proyect

---
+++ interactive_system.js
@@ -0,0 +1,20 @@
+if (0 < userAge && userAge <= 11) {
+    console.log(`Hi ${userName}, you are a kid`)
+    alert(`Hi ${userName}, you are a kid`)
+}
+else if (12 <= userAge && userAge <= 17) {
+    console.log(`Hi ${userName}, you're a minor. keep learning and enjoying code!`)
+    alert(`Hi ${userName}, you're a minor. keep learning and enjoying code!`)
+}
+else if (18 <= userAge && userAge <= 35) {
+    console.log(`Hi ${userName}, you're an adult. Get ready for great oportunities in the world of programming!`)
+    alert(`Hi ${userName}, you're an adult. Get ready for great oportunities in the world of programming!`)
+}
+else if (36 <= userAge && userAge <= 100) {
+    console.log(`Hi ${userName}, enyoy your life.`)
+    alert(`Hi ${userName}, enyoy your life.`)
+}
+else {
+    console.log(`Error: Please, enter a valid age in numbers`)
+    alert(`Error: Please, enter a valid age in numbers`)
+};
